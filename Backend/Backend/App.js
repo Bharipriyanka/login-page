5d02c669273717dced5722b507cbd1b43aea221a
haripriyanka <166783890+Bharipriyanka@users.noreply.github.com>
2024-06-27 11:17:56 +0530

    Add files via upload

---
+++ Backend/Backend/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+import SignInLoginForm from './SignInLoginForm';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Sign In / Login</h1>
+                <SignInLoginForm />
+            </header>
+        </div>
+    );
+}
+
+export default App;
